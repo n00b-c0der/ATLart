735d558bc99d52ee8950661e8865423ecd8d17d1
n00b-c0der <twh710twh@gmail.comm>
2019-04-10 18:54:44 -0400

    more on Ron's excellent updates

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -12,90 +12,49 @@ $(document).ready(function () {
     const guidelist = $(".guides");
 });
 
-const cloudName = 'atlart';
-const unsignedUploadPreset = 'atlart';
+ // this is all from cloudinary  the url to where we will save our images
+ var CLOUDNARY_URL = 'https://api.cloudinary.com/v1_1/ronwab/upload'
+ //upload preset in cloudinary https://cloudinary.com/console/settings/upload  scroll to upload presets 
+ var CLOUDNARY_UPLOAD_PRESET = 'ffghjylr'
+ //imgPreview is an image i had on the screen 
+ var imgPreview = document.getElementById('img-preview');
+ //this creates the thingi to upload the images https://www.w3schools.com/tags/att_input_type_file.asp
+ // I am not sure why they use "for" in the  body tag
+ var fileUpload = document.getElementById('file-upload');
 
-var fileSelect = document.getElementById("fileSelect"),
-  fileElem = document.getElementById("fileElem");
+ //Adding an event listener called change.. I think we could also have used click.. 
+ //This syntax i believe is vanila JS. we can always tweak it to jquery if we need to
+ fileUpload.addEventListener('change', function (event) {
+     event.preventDefault()
 
-fileSelect.addEventListener("click", function(e) {
-  if (fileElem) {
-    fileElem.click();
-  }
-  e.preventDefault(); // prevent navigation to "#"
-}, false);
+     //saving the response back from clicking the button
+     var file = event.target.files[0]
+     //FormData() is built in JS to help create key value pairs that can be used to send data
+     // https://developer.mozilla.org/en-US/docs/Web/API/FormData
+     var formData = new FormData();
+     formData.append('file', file);
+     formData.append('upload_preset', CLOUDNARY_UPLOAD_PRESET)
 
-// ************************ Drag and drop ***************** //
-function dragenter(e) {
-  e.stopPropagation();
-  e.preventDefault();
-}
+     //similar to ajax Tried using ajax but was getting an error that I did not understand :-(
+     // $.ajax({
+     axios({
+         // see line 25
+         url: CLOUDNARY_URL,
+         // POST request pushes img to where ever we are saving
+         method: 'POST',
+         //not sure about this guy
+         headers: {
+             'Content-Type': 'application/x-www-form-urlencoded'
+         },
+         //not sure about this guy
+         data: formData
 
-function dragover(e) {
-  e.stopPropagation();
-  e.preventDefault();
-}
+         //This is just a callback function 
+     }).then(function (res) {
+         console.log(res);
+         // setting returned results to the imgPreview
+         imgPreview.src = res.data.secure_url
+     })
+ })
 
-dropbox = document.getElementById("dropbox");
-dropbox.addEventListener("dragenter", dragenter, false);
-dropbox.addEventListener("dragover", dragover, false);
-dropbox.addEventListener("drop", drop, false);
-
-function drop(e) {
-  e.stopPropagation();
-  e.preventDefault();
-
-  var dt = e.dataTransfer;
-  var files = dt.files;
-
-  handleFiles(files);
-}
-
-// *********** Upload file to Cloudinary ******************** //
-function uploadFile(file) {
-  var url = `https://api.cloudinary.com/v1_1/${atlart}/upload`;
-  var xhr = new XMLHttpRequest();
-  var fd = new FormData();
-  xhr.open('POST', url, true);
-  xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-
-  // Reset the upload progress bar
-   document.getElementById('progress').style.width = 0;
-  
-  // Update progress (can be used to show progress indicator)
-  xhr.upload.addEventListener("progress", function(e) {
-    var progress = Math.round((e.loaded * 100.0) / e.total);
-    document.getElementById('progress').style.width = progress + "%";
-
-    console.log(`fileuploadprogress data.loaded: ${e.loaded},
-  data.total: ${e.total}`);
-  });
-
-  xhr.onreadystatechange = function(e) {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      // File uploaded successfully
-      var response = JSON.parse(xhr.responseText);
-      // https://res.cloudinary.com/cloudName/image/upload/v1483481128/public_id.jpg
-      var url = response.secure_url;
-      // Create a thumbnail of the uploaded image, with 150px width
-      var tokens = url.split('/');
-      tokens.splice(-2, 0, 'w_150,c_scale');
-      var img = new Image(); // HTML5 Constructor
-      img.src = tokens.join('/');
-      img.alt = response.public_id;
-      document.getElementById('gallery').appendChild(img);
-    }
-  };
-
-  fd.append('upload_preset', unsignedUploadPreset);
-  fd.append('tags', 'browser_upload'); // Optional - add tag for image admin in Cloudinary
-  fd.append('file', file);
-  xhr.send(fd);
-}
-
-// *********** Handle selected files ******************** //
-var handleFiles = function(files) {
-  for (var i = 0; i < files.length; i++) {
-    uploadFile(files[i]); // call the function to upload the file
-  }
-};
+ 
