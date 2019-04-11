2bb81eff58f4ce57aee7cf959f3ec1c8be4d392a
ronaldwabukenda <ronald.wabukenda@salesloft.com>
2019-04-11 19:36:39 -0400

    made changes to the firebase file

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,30 +1,39 @@
 $(document).ready(function () {
+    console.log("test1");
 
     //setup materialize
+
+
+
+    // var guidelist2 =  document.querySelector('.guides')
+    // accesses the dom for guide objects
+    const guidelist = $(".guides");
+
+    // setup guide
+    window.setupGuides = (data) => {
+        let html = '';
+        data.forEach(function doc() {
+            const guide = doc.data()
+            console.log(guide);
+
+        })
+        // setupGuides(snapshot.docs)
+        // https: www.youtube.com/watch?v=kIVg_yTiO3Q 9.54
+
+    }
     var modals = document.querySelectorAll(".modal");
     M.Modal.init(modals);
 
     var items = document.querySelectorAll(".collapsible");
     M.Collapsible.init(items);
 
+    const loggedOutLinks = $('.logged-out');
+    const logggedInLinks = $('.linked-in')
+    const setupUI = (user) => {
+        if (user) {
 
-    // var guidelist2 =  document.querySelector('.guides')
-    const guidelist = $(".guides");
-
-    // setup guidelist 
-    // const setupGuides = (data) => {
-    //     let html = '',
-    // https: www.youtube.com/watch?v=kIVg_yTiO3Q 9.54
-
-    // }
+        }
+    }
 
 });
-
-// var tregex =/[0-9]{3}(-|.)[0-9]{3}(.|-)[0-9]{4}/g
-// var texting =//
-// var num = 12345678
-// if (num.value.Match(tregex)) {
-//     console.log("true")
-// } else {
-//     console.log("false")
-// }
+console.log("test");
